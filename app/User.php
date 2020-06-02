2d85f54f18b110e100de9b3dee53e290eb2ce712
imbattle <imbattle@gmail.com>
2020-06-02 14:41:13 +0800

    Revert "create_tables *3"
    This reverts commit bb5a78d550302752c5b8f453f686138b15688b4b.

--- app/User.php
+++ app/User.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\User;
+namespace App;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
@@ -14,21 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            //會員編號
             $table->bigIncrements('id');
-            //會員姓名
             $table->string('name');
-            //Email+鍵值
-            $table->string('email',150)->unique();
-
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            //密碼
-            $table->string('password',60);
-            //帳號類型，A(Admin，管理者)，G(General,一般會員)
-            $table->string('type',1)->default('G');//帳號狀態
-
-            //設為允許null
-            $table->rememberToken()->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
